a0e6af07d36c4e48a2c6d747154d890c71215bf3
Matthew Lyon <matthew@lyonheart.us>
2019-12-29 22:00:01 -0800

    first

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Soundplane for Bitwig
+
+This is a [control script](https://www.bitwig.com/en/community/control_scripts.html) for [Bitwig Studio](https://bitwig.com) to enable [MPE](https://support.roli.com/support/solutions/articles/36000027933-what-is-mpe-) for the [Madrona Labs Soundplane](https://madronalabs.com/soundplane).
+
+## Installation & Setup
+
+This assumes you're using the [Madrona Soundplane Client](https://github.com/madronalabs/soundplane) software, which is currently supported on Macintosh. It can apparently compile on Linux but it is officially untested and I have not attempted this. This script currently assumes you're using the Mac app.
+
+It also assumes you're using a Soundplane mapping config that outputs midi notes, such as `rows in fourths`.
+
+1. download `Soundplane.control.js` and put it in the `Documents/Bitwig Studio/Controller Scripts` directory under your home directory.
+
+2. Launch `Soundplane` and set its MIDI output to the following:
+
+    a. active: on
+    b. bend range: if it's set to `48`, pitch bend maps to the range of the Soundplane
+    c. pressure: on
+    d. MPE: on
+    e. device: `Soundplane IAC out`
+
+3. In Bitwig's preferences panel, under `Settings`, `Controllers`, click `Add Controller`, find `Madrona Labs`, `Soundplane`, and click `Add`. It should find the midi port automatically.
+
+## Mappings
+
+- X-axis: maps to pitch/micro-pitch. If the Soundplane Client App's `bend range` parameter is set to `48` as recommended above, gldiding across notes should be seamless.
+- The Y-axis maps to what Bitwig calls `TIMB` under the `Expressions` modulator.
+- Pressure will map to what Bitwig calls `PRES` under the `Expressions` modulator.
---
+++ Soundplane.control.js
@@ -0,0 +1,52 @@
+loadAPI(10);
+
+// Remove this if you want to be able to use deprecated methods without causing script to stop.
+// This is useful during development.
+host.setShouldFailOnDeprecatedUse(true);
+
+host.defineController("Madrona Labs", "Soundplane", "0.1", "29d86405-bd65-4840-aaca-61b0a4e290e8", "mattly");
+
+host.defineMidiPorts(1, 0);
+
+if (host.platformIsWindows())
+{
+   // TODO: Set the correct names of the ports for auto detection on Windows platform here
+   // and uncomment this when port names are correct.
+   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0"], ["Output Port 0", "Output Port -1"]);
+}
+else if (host.platformIsMac())
+{
+   // TODO: Set the correct names of the ports for auto detection on Mac OSX platform here
+   // and uncomment this when port names are correct.
+   host.addDeviceNameBasedDiscoveryPair(["Soundplane IAC out"], []);
+}
+else if (host.platformIsLinux())
+{
+   // TODO: Set the correct names of the ports for auto detection on Linux platform here
+   // and uncomment this when port names are correct.
+   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0"], ["Output Port 0", "Output Port -1"]);
+}
+
+function init() {
+  host.getMidiInPort(0).setMidiCallback(onMidi);
+
+  generic = host.getMidiInPort(0).createNoteInput("", "??????")
+  generic.setUseExpressiveMidi(true, 0, 48)
+  generic.setShouldConsumeEvents(false)
+
+   // TODO: Perform further initialization here.
+   println("Soundplane initialized!");
+}
+
+// Called when a short MIDI message is received on MIDI input port 0.
+function onMidi(status, data1, data2) {
+   // TODO: Implement your MIDI input handling code here.
+}
+
+function flush() {
+   // TODO: Flush any output to your controller here.
+}
+
+function exit() {
+
+}
